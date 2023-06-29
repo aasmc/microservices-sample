94849e3fc4b4e918c4e32dd7302fc7eafca93325
Alex Alexeyev <aasmc@yandex.ru>
2023-06-29 22:32:11 +0300

    feat: add inventory-service logic

---
+++ inventory-service/src/main/java/ru/aasmc/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package ru.aasmc.inventoryservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.aasmc.inventoryservice.model.Inventory;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
---
+++ inventory-service/src/test/java/ru/aasmc/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.aasmc.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InventoryServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
